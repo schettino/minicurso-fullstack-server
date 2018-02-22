286eb7748cc660c6c576fce79229e03278816bca
Matheus Schettino <schetitno2@gmail.com>
2018-02-22 09:18:28 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Env vars
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config();
+}
+
+// Deps
+const Hapi = require('hapi');
+const Logger = require('winston');
+
+// Constants
+const { PORT } = process.env;
+const port = PORT || 4444;
+
+// Server instance
+const server = new Hapi.Server({ port });
+
+// Run
+(async function() {
+  try {
+    await server.start();
+    Logger.info(`Server started on port ${port}`);
+  } catch (error) {
+    Logger.error('There was an error on server initialization', { error });
+  }
+})();
